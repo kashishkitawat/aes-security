fda434288f40d4ec7eb51e58781f123da0267f1e
kashishkitawat <kitawat.kashish@gmail.com>
2020-03-26 17:54:25 -0500

    cosmetic changes

--- iotSec.py
+++ iotSec.py
@@ -4,6 +4,7 @@ from getpass import getpass
 from pathlib import Path
 from Crypto.Cipher import AES
 from pandas import DataFrame
+import re
 
 key = b'1234567890qwerty'  # size = 16
 
@@ -42,11 +43,6 @@ class Encryptor:
         """
         Encrypt data.
 
-        Step:
-            1. Padding the data to make it's size 16.
-            2. Create a cipher obj, which will encrypt the data using the key.
-            3. Using the cipher object, encrypt the data.
-
         Returns:
             encrypted data (byte)
         """
@@ -55,14 +51,9 @@ class Encryptor:
         encrypted_text = cipher.encrypt(message)
         return encrypted_text
 
-    def _encrypt_file(self, file_name, data=False, print_data=False):
+    def encrypt_file(self, file_name, data=False, print_data=False):
         """
         Encrypt file.
-
-        Steps:
-            1. Open the file and read the data
-            2. Encrypt the data of the files
-            3. Write the data in the file
 
         Returns:
             None
@@ -82,12 +73,6 @@ class Encryptor:
         """
         Decrypt data.
 
-        Steps:
-            1. Create the cipher object using the key
-            2. Decrypt the data using cipher object and decode it from
-               bytes to string format
-            3. Remove the extra characters used while encrypting
-
         Returns:
             Raw data (str)
         """
@@ -95,14 +80,9 @@ class Encryptor:
         plaintext = cipher.decrypt(cipher_text).decode('utf-8')
         return self._unpading(plaintext)
 
-    def _decrypt_file(self, file_name, rtn_data=False, print_data=False):
+    def decrypt_file(self, file_name, rtn_data=False, print_data=False):
         """
         Decrypt File.
-
-        Steps:
-            1. Read the file and store the encrypted data
-            2. decrypt the data, using the key
-            3. Write the decrypted data and store the file
 
         Returns:
             None
@@ -114,21 +94,17 @@ class Encryptor:
             print(dcrypt)
         if rtn_data:
             return dcrypt
-        # with open(file_name, 'w') as f:
-        #   f.write(dcrypt)
 
     def check_usr(self, usr, paswrd):
         """
         Authenticate user.
 
         Returns:
-            group name or 0
+            Group Name or 0
         """
-        data = self._decrypt_file(self.passFile, rtn_data=True)
+        data = self.decrypt_file(self.passFile, rtn_data=True)
 
         for line in data.strip().split('\n'):
-            # print(line)
-            # print(line.split(' '))
             username, password, group = line.split(' ')
             if usr == username and paswrd == password:
                 return group
@@ -141,7 +117,7 @@ class Encryptor:
         Returns:
             List of allowed access
         """
-        data = self._decrypt_file(self.roleFile, rtn_data=True)
+        data = self.decrypt_file(self.roleFile, rtn_data=True)
 
         for line in data.split('\n'):
             if group in line:
@@ -156,7 +132,7 @@ class Encryptor:
         Returns:
             None
         """
-        data = self._decrypt_file(self.dataFile, rtn_data=True)
+        data = self.decrypt_file(self.dataFile, rtn_data=True)
         data = data.strip().split("\n")
         data = [data[i].split(",") for i in range(len(data))]
         headers = data.pop(0)
@@ -167,28 +143,25 @@ class Encryptor:
 
 if __name__ == '__main__':
     enc = Encryptor(key)
-    # enc._encrypt_file('data.csv', print_data=True)
-    # enc._decrypt_file('passwords.txt.enc', print_data=True)
-    """
-        Need to add more input parts
-    """
     if not Path('passwords.txt.enc').is_file():
         username = input('Enter Admin Username: ')
         password = getpass('Enter Admin Password: ')
         with open('passwords.txt', 'w+') as f:
             f.write(username + ' ' + password + ' admin')
-        enc._encrypt_file('passwords.txt')
+        enc.encrypt_file('passwords.txt')
+        Path('passwords.txt').unlink()
         if not Path('roles.txt.enc').is_file():
             print('Enter admin roles in the form of')
             print('column1, column2, column3')
             admin_roles = input('Enter roles: ')
             with open('roles.txt', 'w+') as f:
                 f.write('admin = ' + admin_roles)
-            enc._encrypt_file('roles.txt')
+            enc.encrypt_file('roles.txt')
+            Path('roles.txt').unlink()
         if not Path('data.csv').is_file():
-            print('Please save your data.csv file in here')
+            print('!!!Please save your data.csv file in here!!!')
         else:
-            enc._encrypt_file('data.csv')
+            enc.encrypt_file('data.csv')
     else:
         usr = input('Enter Username: ')
         paswd = getpass()
@@ -211,9 +184,12 @@ if __name__ == '__main__':
                 else:
                     print('Wrong option')
                     exit()
-                data = enc._decrypt_file('passwords.txt', rtn_data=True)
+                data = enc.decrypt_file('passwords.txt', rtn_data=True)
                 data = data.split('\n')
                 modified = False
+                if len(add_data.split()) != 3:
+                    print('ERROR: Details not added properly. Try again!')
+                    exit()
                 for i in range(len(data)):
                     if add_data.split()[0] in data[i]:
                         data[i] = add_data
@@ -221,9 +197,10 @@ if __name__ == '__main__':
                 if not modified:
                     data.append(add_data)
                 data = '\n'.join(data)
-                with open('passwords.txt', 'w') as f:
+                with open('passwords.txt', 'w+') as f:
                     f.write(data)
-                enc._encrypt_file('passwords.txt')
+                enc.encrypt_file('passwords.txt')
+                Path('passwords.txt').unlink()
             elif choice is '2':
                 print('1. Add roles')
                 print('2. Modify roles')
@@ -237,9 +214,16 @@ if __name__ == '__main__':
                 else:
                     print('Wrong option')
                     exit()
-                data = enc._decrypt_file('roles.txt', rtn_data=True)
+                data = enc.decrypt_file('roles.txt', rtn_data=True)
                 data = data.split('\n')
                 modified = False
+                regex_match_1 = re.compile('.*=.*')
+                regex_match_2 = re.compile('.*=.*,.*')
+                match_1 = regex_match_1.match(add_data)
+                match_2 = regex_match_2.match(add_data)
+                if (match_1 and match_2) is None:
+                    print('ERROR: Details not added properly. Try Again!')
+                    exit()
                 for i in range(len(data)):
                     if add_data.split()[0] in data[i]:
                         data[i] = add_data  # replace that line with new data
@@ -247,9 +231,10 @@ if __name__ == '__main__':
                 if not modified:
                     data.append(add_data)
                 data = '\n'.join(data)
-                with open('roles.txt', 'w') as f:
+                with open('roles.txt', 'w+') as f:
                     f.write(data)
-                enc._encrypt_file('roles.txt')
+                enc.encrypt_file('roles.txt')
+                Path('roles.txt').unlink()
             elif choice is '3':
                 if not Path('data.csv.enc').is_file():
                     print('Cannot process the request because \n\
@@ -262,9 +247,7 @@ if __name__ == '__main__':
                 exit()
         else:
             if group != 0:
-                # print(group)
                 can_be_accessed = enc.get_roles(group)
-                # print(can_be_accessed)
                 enc.get_data(can_be_accessed)
             else:
                 print('Access denied')
